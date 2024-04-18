15822788dab0789d47a97e45544fc09d3736bf7b
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-04-18 10:08:43 -0400

    cambios

---
+++ app/Controllers/Historico_examenesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use App\core\SessionManager;
+use App\core\Token;
+use App\core\Request;
+use App\core\View;
+use App\core\Redirect;
+use App\core\DB;
+use Xinvoice;
+
+class Historico_examenesController
+{
+    public $token;
+
+	public function __construct()
+	{
+		SessionManager::startSession();
+		$Sesusuario = SessionManager::get('usuario');
+		if (!isset($Sesusuario)) {
+			Redirect::to("login/index");
+		}
+        $this->token = Token::generateFormToken('send_message');
+	}
+
+    public function index()
+    {
+        View::render('historico_examenes');
+    }
+}
