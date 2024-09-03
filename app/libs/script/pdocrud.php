2162ca08c6cfb8269738aa2ab2d620891e2ed908
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-03 16:37:27 -0400

    up

--- app/libs/script/pdocrud.php
+++ app/libs/script/pdocrud.php
@@ -1,15 +1,18 @@
 <?php
-date_default_timezone_set(@date_default_timezone_get());
-define('PDOCrudABSPATH', dirname(__FILE__) . '/');
-require_once PDOCrudABSPATH . "../../../vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+
+// Cargar variables de entorno antes de iniciar la sesión
 $dotenv = DotenvVault\DotenvVault::createImmutable(dirname(__DIR__, 3));
 $dotenv->safeLoad();
-@session_name($_ENV['APP_NAME']);
+
+@session_name($_ENV["APP_NAME"]);
 @session_start();
 /*enable this for development purpose */
 //ini_set('display_startup_errors', 1);
 //ini_set('display_errors', 1);
 //error_reporting(-1);
+date_default_timezone_set(@date_default_timezone_get());
+define('PDOCrudABSPATH', dirname(__FILE__) . '/');
 require_once PDOCrudABSPATH . "config/config.php";
 spl_autoload_register('pdocrudAutoLoad');
 
