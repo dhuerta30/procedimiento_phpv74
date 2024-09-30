33e9f9dbba2f992e3a51314c315ea45be6c2e37c
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-30 11:04:20 -0300

    up

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -32,15 +32,19 @@ class LoginController {
     public function index(){
         $pdocrud = DB::PDOCrud();
 		$pdocrud->addPlugin("bootstrap-inputmask");
+		$pdocrud->fieldCssClass("usuario", array("usuario"));
 		$pdocrud->fieldCssClass("rut", array("rut"));
 		$pdocrud->fieldCssClass("password", array("password"));
-		$pdocrud->fieldDisplayOrder(array("rut", "password"));
-		$pdocrud->fieldRenameLable("email", "Correo");
+		$pdocrud->fieldDisplayOrder(array("usuario", "rut", "password"));
+		$pdocrud->fieldGroups("Name",array("usuario","rut"));
+		$pdocrud->fieldNotMandatory("usuario");
+		$pdocrud->fieldNotMandatory("rut");
 		$pdocrud->fieldRenameLable("password", "Contraseña");
-        $pdocrud->fieldAddOnInfo("rut", "before", '<div class="input-group-append"><span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span></div>');
+        $pdocrud->fieldAddOnInfo("usuario", "before", '<div class="input-group-append"><span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span></div>');
+		$pdocrud->fieldAddOnInfo("rut", "before", '<div class="input-group-append"><span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span></div>');
         $pdocrud->fieldAddOnInfo("password", "before", '<div class="input-group-append"><span class="input-group-text" id="basic-addon1"><i class="fa fa-key"></i></span></div>');
 		$pdocrud->addCallback("before_select", "beforeloginCallback");
-		$pdocrud->formFields(array("rut", "password"));
+		$pdocrud->formFields(array("usuario", "rut", "password"));
 		$pdocrud->setLangData("login", "Ingresar");
 		$login = $pdocrud->dbTable("usuario")->render("selectform");
 		$mask = $pdocrud->loadPluginJsCode("bootstrap-inputmask",".rut", array("mask"=> "'9{1,2}9{3}9{3}-(9|k|K)'", "casing" => "'upper'", "clearIncomplete" => "true", "numericInput"=> "true", "positionCaretOnClick" => "'none'"));
