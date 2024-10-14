500c27e631909ded7e76685fab87b9e14ea2ec51
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-14 11:50:18 -0300

    up

--- app/Views/busqueda_por_rut.php
+++ app/Views/busqueda_por_rut.php
@@ -1,5 +1,9 @@
+
 <?php require "layouts/header.php"; ?>
 <?php require "layouts/sidebar.php"; ?>
+<link href="<?=$_ENV["BASE_URL"]?>css/sweetalert2.min.css" rel="stylesheet">
+<link rel="stylesheet" href="<?=$_ENV["BASE_URL"]?>app/libs/script/plugins/datatable/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="<?=$_ENV["BASE_URL"]?>css/buttons.dataTables.min.css">
 <div class="content-wrapper">
     <section class="content">
         <div class="card mt-4">
@@ -7,8 +11,53 @@
 
                 <div class="row procedimiento">
                     <div class="col-md-12">
-                        <h5>titulo</h5>
-                        <hr>
+
+                        <div class="row mb-3">
+                            <div class="col-md-12">
+                                <form id="form1" name="form1" action="rango.php" onsubmit="return validar();">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <input class="form-control" type="date" name="ingreso" id="ingreso" title="Ingrese fecha de Inicio de Busqueda">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <input class="form-control" type="date" name="termino" id="termino" title="Ingrese Fecha de Termino de Busqueda"> 
+                                        </div>
+                                        <div class="col-md-4">
+                                            <input type="submit" id="enviar" name="enviar" value="Buscar" class="btn btn-primary" title="Buscar" style="cursor:hand">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+
+                            <div class='table-responsive tabla_principal'>
+                                <table class="table table-striped tabla_por_rut text-center" style="width:100%">
+                                    <thead class="bg-primary">
+                                        <tr>
+                                            <th>N° Rut</th>
+                                            <th>Código</th>
+                                            <th>Nombre</th>
+                                            <th>Apellido Paterno</th>
+                                            <th>Apellido Materno</th>
+                                            <th>Fecha Estudio</th>
+                                            <th>Estudio</th>
+                                            <th>Observaciones</th>
+                                            <th>Fecha Registro</th>
+                                            <th>Ver</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
 
@@ -19,4 +68,61 @@
 <div id="pdocrud-ajax-loader">
     <img width="300" src="<?=$_ENV["BASE_URL"]?>app/libs/script/images/ajax-loader.gif" class="pdocrud-img-ajax-loader"/>
 </div>
+<script src="<?=$_ENV["BASE_URL"]?>app/libs/script/js/jquery.min.js"></script>
+<script src="<?=$_ENV["BASE_URL"]?>js/sweetalert2.all.min.js"></script>
+<script src="<?=$_ENV["BASE_URL"]?>js/flatpickr.js"></script>
+<script src="<?=$_ENV["BASE_URL"]?>app/libs/script/plugins/datatable/js/jquery.dataTables.min.js"></script>
+<script src="<?=$_ENV["BASE_URL"]?>js/moment.min.js"></script>
+<script src="<?=$_ENV["BASE_URL"]?>js/pdfmake.min.js"></script>
+<script>
+$(document).ready(function() {
+    $.ajax({
+        type: "POST",
+        url: "<?=$_ENV['BASE_URL']?>Busqueda/listar_rango_tabla_nulla",
+        dataType: "json",
+        success: function(data) {
+            // Verifica si hay datos y luego inicializa la DataTable
+            if (data.data && data.data.length > 0) {
+                // Crear un array para los datos de DataTable
+                let tableData = data.data.map(item => [
+                    item.rut,
+                    item.codigo,
+                    item.nombre,
+                    item.apaterno,
+                    item.amaterno,
+                    item.fechaestudio,
+                    item.estudio,
+                    item.observaciones,
+                    item.fecharegistro,
+                    item.ver
+                ]);
+
+                // Inicializa la DataTable con los datos
+                $(".tabla_por_rut").DataTable({
+                    data: tableData,
+                    columns: [
+                        { title: "rut" },     // Títulos de las columnas
+                        { title: "codigo" },
+                        { title: "nombre" },
+                        { title: "apaterno" },
+                        { title: "amaterno" },
+                        { title: "fechaestudio" },
+                        { title: "estudio" },
+                        { title: "observaciones" },
+                        { title: "fecharegistro" },
+                        { title: "ver" }
+                    ]
+                });
+            } else {
+                console.log("No hay datos para mostrar.");
+            }
+        },
+        error: function(xhr, status, error) {
+            // Manejo de errores
+            console.error("Error en la solicitud AJAX:", status, error);
+        }
+    });
+});
+
+</script>
 <?php require "layouts/footer.php"; ?>
