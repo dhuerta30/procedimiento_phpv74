3bbb92642eb57ac1ad811e285f7b56d45302ac23
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-28 12:29:38 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2666,7 +2666,7 @@ class HomeController
 			$id = $request->post('id');
 
 			$pdomodel = $pdocrud->getPDOModelObj();
-			$pdomodel->columns = array("datos_paciente.id_datos_paciente", "fecha_solicitud", "observacion");
+			$pdomodel->columns = array("datos_paciente.id_datos_paciente", "fecha_solicitud", "tipo_solicitud", "examen", "observacion");
 			$pdomodel->joinTables("detalle_de_solicitud", "detalle_de_solicitud.id_datos_paciente = datos_paciente.id_datos_paciente", "INNER JOIN");
 
 			$pdomodel->where("datos_paciente.id_datos_paciente", $id, "=", "AND");
@@ -2675,13 +2675,13 @@ class HomeController
 			$pdomodel->where("id_datos_paciente", $id, "=", "AND");
 			$diagnostico_antecedentes_paciente = $pdomodel->select("diagnostico_antecedentes_paciente");
 
-			print_r($diagnostico_antecedentes_paciente);
-
 			$paciente = new DatosPacienteModel();
 			$data = $paciente->PacientePorId($id);
 		
 			$pdocrud->formFieldValue("id_datos_paciente", $id_datos_paciente[0]["id_datos_paciente"]);
 			$pdocrud->formFieldValue("fecha_solicitud", $id_datos_paciente[0]["fecha_solicitud"]);
+			$pdocrud->formFieldValue("tipo_solicitud", $id_datos_paciente[0]["tipo_solicitud"]);
+			$pdocrud->formFieldValue("examen", $id_datos_paciente[0]["examen"]);
 
 			$pdocrud->formFieldValue("especialidad", $diagnostico_antecedentes_paciente[0]["especialidad"]);
 
