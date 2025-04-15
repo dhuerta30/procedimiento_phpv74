e21c873e99af43e3051598844b815813430f680c
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-04-15 16:48:38 -0400

    up

--- app/Views/buscar_polos.php
+++ app/Views/buscar_polos.php
@@ -212,10 +212,10 @@ $(document).on("click", ".buscar", function(event) {
                                 "previous": "Anterior"
                             }
                         },
-                        data: response.data, // Los datos filtrados del controlador PHP
+                        data: response.data,
                         destroy: true,
                         columns: [
-                            { data: 'rut' }, // Ensure this key exists in the returned objects
+                            { data: 'rut' },
                             { data: 'poc' },
                             { data: 'dnombre' },
                             { data: 'apellidop' },
